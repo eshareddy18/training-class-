5eceb8c8b3ac99a66cc3c0044e42fa04ca1e3689
eshareddy18 <2311cs020251@mallareddyuniversity.ac.in>
2025-01-09 04:43:24 +0000

    lambdafunction

---
+++ lambdafun.py
@@ -0,0 +1,24 @@
+result = lambda x,y:x+y
+print(result(3,5))
+
+##iterable
+lst=[1,2,3,4]
+for i in lst:
+    print(i)
+
+#without generator
+def square(i):
+    for i in range(i):
+        i ** 2
+    return i
+res = square(3)
+print(res)
+
+#iterable
+lst=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+for i in lst:
+    print(i)
+
+
+
+
